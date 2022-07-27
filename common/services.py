317dd821a74326815d96ed52ad48dfafa7c0e7a0
adhil <adhil1998@gmail.com>
2022-07-27 05:30:08 +0530

    secret key

--- common/services.py
+++ common/services.py
@@ -1,22 +1,11 @@
-from twilio.rest import Client
+import os
 import requests
-# Your Account SID from twilio.com/console
-# account_sid = "AC406c9f456a9275d7d3a66344c053eca7"
-# auth_token = "d78fd1f23ba63de1205abbfac23b3623"
-# client = Client(account_sid, auth_token)
+from common.exceptions import BadRequest
 
-
-# def send_otp(to, body):
-#     message = client.messages.create(
-#         to=to,
-#         from_="+18434387495",
-#         body=body)
-#     print(message.sid)
 
 def send_otp(to, body):
     url = "https://www.fast2sms.com/dev/bulkV2"
-
-    querystring = {"authorization": "qpO4uwJhAMyZWeDSVvgtUC5cH7fNsTPXFxzb2lGjQ1i80rnE3abscB472Z6GOgVktxEYQhTFSlRen5Mm",
+    querystring = {"authorization": os.environ['fas2sms_api'],
                    "message": body,
                    "language": "english",
                    "route": "q",
